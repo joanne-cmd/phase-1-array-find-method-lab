0965b182287adffca4ca94ca338641911b377d35
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2022-11-28 03:31:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+const superbowlWin = objs => {
+    let obj = (objs.find(obj => {
+        return obj.result==="W" ? true : false
+    }))
+
+    return obj ? obj.year : undefined
+}
